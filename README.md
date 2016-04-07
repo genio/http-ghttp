d58b3f4cabcb6dd56c5ce54c3f1ff82357c0d162
Chase Whitener <chase.whitener@infotechfl.com>
2016-04-07 17:23:46 -0400

    fixed up documentation a bit for a test release

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # NAME
 
-HTTP::GHTTP - Perl interface to the gnome ghttp library
+HTTP::GHTTP - (DEPRECATED) Perl interface to the gnome ghttp library
 
 # SYNOPSIS
 
+    use strict;
+    use warnings;
     use HTTP::GHTTP;
 
     my $r = HTTP::GHTTP->new();
@@ -13,157 +15,201 @@ HTTP::GHTTP - Perl interface to the gnome ghttp library
 
 # DESCRIPTION
 
-This is a fairly low level interface to the Gnome project's libghttp,
+The GNOME [libghttp](http://ftp.gnome.org/pub/gnome/sources/libghttp) project
+has been dead since 2009.  This, this library is deprecated.
+
+**Do not use this module!**
+
+This is a fairly low level interface to [libghttp](http://ftp.gnome.org/pub/gnome/sources/libghttp),
 which allows you to process HTTP requests to HTTP servers. There also
 exists a slightly higher level interface - a simple get() function
 which takes a URI as a parameter. This is not exported by default, you
 have to ask for it explicitly.
 
-# API
+# FUNCTIONS
 
-## HTTP::GHTTP->new(\[$uri, \[%headers\]\])
+The [HTTP::GHTTP](https://metacpan.org/pod/HTTP::GHTTP) module makes the following standalone functions available.
+
+## get
+
+    get($uri, [%headers])
+
+This does everything automatically for you, retrieving the body at
+the remote URI. Optionally pass in headers.
+
+# METHODS
+
+The [HTTP::GHTTP](https://metacpan.org/pod/HTTP::GHTTP) module makes the following methods available.
+
+## new
+
+    my $r = HTTP::GHTTP->new("http://axkit.com/", Connection => "close");
 
 Constructor function - creates a new GHTTP object. If supplied a URI
 it will automatically call set\_uri for you. If you also supply a list
 of key/value pairs it will set those as headers:
 
-    my $r = HTTP::GHTTP->new(
-        "http://axkit.com/",
-        Connection => "close");
+## current\_status
 
-## $r->set\_uri($uri)
-
-This sets the URI for the request
-
-## $r->set\_header($header, $value)
-
-This sets an outgoing HTTP request header
-
-## $r->set\_type($type)
-
-This sets the request type. The request types themselves are constants
-that are not exported by default. To export them, specify the :methods
-option in the import list:
-
-    use HTTP::GHTTP qw/:methods/;
-    my $r = HTTP::GHTTP->new();
-    $r->set_uri('http://axkit.com/');
-    $r->set_type(METHOD_HEAD);
-    ...
-
-The available methods are:
-
-    METHOD_GET
-    METHOD_POST
-    METHOD_OPTIONS
-    METHOD_HEAD
-    METHOD_PUT
-    METHOD_DELETE
-    METHOD_TRACE
-    METHOD_CONNECT
-    METHOD_PROPFIND
-    METHOD_PROPPATCH
-    METHOD_MKCOL
-    METHOD_COPY
-    METHOD_MOVE
-    METHOD_LOCK
-    METHOD_UNLOCK
-
-Some of these are for DAV.
-
-## $r->set\_body($body)
-
-This sets the body of a request, useful in POST and some of the DAV
-request types.
-
-## $r->process\_request()
-
-This sends the actual request to the server
-
-## $r->get\_status()
-
-This returns 2 values, a status code (numeric) and a status reason
-phrase. A simple example of the return values would be (200, "OK").
-
-## $r->get\_header($header)
-
-This gets the value of an incoming HTTP response header
-
-## $r->get\_headers()
-
-Returns a list of all the response header names in the order they
-came back. This method is only available in libghttp 1.08 and later -
-perl Makefile.PL should have reported whether it found it or not.
-
-    my @headers = $r->get_headers;
-    print join("\n",
-          map { "$_: " . $r->get_header($_) } @headers), "\n\n";
-
-## $r->get\_body()
-
-This gets the body of the response
-
-## $r->get\_error()
-
-If the response failed for some reason, this returns a textual error
-
-## $r->set\_authinfo($user, $password)
-
-This sets an outgoing username and password for simple HTTP
-authentication
-
-## $r->set\_proxy($proxy)
-
-This sets your proxy server, use the form "http://proxy:port"
-
-## $r->set\_proxy\_authinfo($user, $password)
-
-If you have set a proxy and your proxy requires a username and password
-you can set it with this.
-
-## $r->prepare()
-
-This is a low level interface useful only when doing async downloads.
-See ["ASYNC OPERATION"](#async-operation).
-
-## $r->process()
-
-This is a low level interface useful only when doing async downloads.
-See ["ASYNC OPERATION"](#async-operation).
-
-process returns undef for error, 1 for "in progress", and zero for
-"complete".
-
-## $r->get\_socket()
-
-Returns an IO::Handle object that is the currently in progress socket.
-Useful only when doing async downloads. There appears to be some corruption
-when using the socket to retrieve file contents on more recent libghttp's.
-
-## $r->current\_status()
+    $r->current_status()
 
 This is only useful in async mode. It returns 3 values: The current
 processing stage (0 = none, 1 = request, 2 = response headers,
 3 = response), the number of bytes read, and the number of bytes total.
 
-## $r->set\_async()
+## get\_body
+
+    my $rresponse = $r->get_body();
+
+This gets the body of the response
+
+## get\_error
+
+    my $error = $r->get_error();
+
+If the response failed for some reason, this returns a textual error.
+
+## get\_header
+
+    my $value = $r->get_header($header);
+
+This gets the value of an incoming HTTP response header.
+
+## get\_headers
+
+    my @headers = $r->get_headers;
+    print join("\n",
+      map { "$_: " . $r->get_header($_) } @headers), "\n\n";
+
+Returns a list of all the response header names in the order they
+came back. This method is only available in [libghttp](http://ftp.gnome.org/pub/gnome/sources/libghttp) 1.08 and later -
+`perl Makefile.PL` should have reported whether it found it or not.
+
+## get\_socket
+
+    my $sock_h = $r->get_socket();
+
+Returns an [IO::Handle](https://metacpan.org/pod/IO::Handle) object that is the currently in progress socket.
+Useful only when doing async downloads. There appears to be some corruption
+when using the socket to retrieve file contents on more recent [libghttp](http://ftp.gnome.org/pub/gnome/sources/libghttp).
+
+## get\_status
+
+    $r->get_status()
+
+This returns 2 values, a status code (numeric) and a status reason
+phrase. A simple example of the return values would be (200, "OK").
+
+## prepare
+
+    $r->prepare()
+
+This is a low level interface useful only when doing async downloads.
+See ["ASYNC OPERATION"](#async-operation).
+
+## process
+
+    $r->process()
+
+This is a low level interface useful only when doing async downloads.
+See ["ASYNC OPERATION"](#async-operation).
+
+process returns `undef` for error, 1 for "in progress", and zero for
+"complete".
+
+## process\_request
+
+    $r->process_request()
+
+This sends the actual request to the server
+
+## set\_async
+
+    $r->set_async()
 
 This turns async mode on. There is no corresponding unset function.
 
-## $r->set\_chunksize($bytes)
+## set\_authinfo
+
+    $r->set_authinfo($user, $password)
+
+This sets an outgoing username and password for simple HTTP
+authentication.
+
+## set\_body
+
+    $r->set_body($body);
+
+This sets the body of a request, useful in `POST` and some of the DAV
+request types.
+
+## set\_chunksize
+
+    $r->set_chunksize($bytes)
 
 Sets the download (and upload) chunk size in bytes for use in async
 mode. This may be a useful value to set for slow modems, or perhaps
 for a download progress bar, or just to allow periodic writes.
 
-## get($uri, \[%headers\])
+## set\_header
 
-This does everything automatically for you, retrieving the body at
-the remote URI. Optionally pass in headers.
+    $r->set_header($header_name, $header_value);
+    $r->set_header('Connection', 'Close');
+
+This sets an outgoing HTTP request header.
+
+## set\_proxy
+
+    $r->set_proxy("http://proxy:port")
+
+This sets your proxy server.
+
+## set\_proxy\_authinfo
+
+    $r->set_proxy_authinfo($user, $password)
+
+If you have set a proxy and your proxy requires a username and password
+you can set it with this.
+
+## set\_type
+
+    use HTTP::GHTTP qw/:methods/;
+    my $r = HTTP::GHTTP->new();
+    $r->set_uri('http://axkit.com/');
+    $r->set_type(METHOD_HEAD);
+
+This sets the request type. The request types themselves are constants
+that are not exported by default. To export them, specify the `:methods`
+option in the import list:
+
+The available methods are:
+
+- METHOD\_GET
+- METHOD\_POST
+- METHOD\_OPTIONS
+- METHOD\_HEAD
+- METHOD\_PUT
+- METHOD\_DELETE
+- METHOD\_TRACE
+- METHOD\_CONNECT
+- METHOD\_PROPFIND
+- METHOD\_PROPPATCH
+- METHOD\_MKCOL
+- METHOD\_COPY
+- METHOD\_MOVE
+- METHOD\_LOCK
+- METHOD\_UNLOCK
+
+## set\_uri
+
+    my $boolean = $r->set_uri($some_URI_string);
+
+This sets the URI for the request.  It returns true if the URI was
+properly set and undef otherwise.
 
 # ASYNC OPERATION
 
-Its possible to use an asynchronous mode of operation with ghttp. Here's
+It's possible to use an asynchronous mode of operation with [HTTP::GHTTP](https://metacpan.org/pod/HTTP::GHTTP). Here's
 a brief example of how:
 
     my $r = HTTP::GHTTP->new("http://axkit.org/");
@@ -185,10 +231,10 @@ a brief example of how:
 Doing timeouts is an exercise for the reader (hint: lookup select() in
 perlfunc).
 
-Note also that $sock above is an IO::Handle, not an IO::Socket, although
+Note also that $sock above is an [IO::Handle](https://metacpan.org/pod/IO::Handle), not an [IO::Socket](https://metacpan.org/pod/IO::Socket), although
 you can probably get away with re-blessing it. Also note that by calling
-$r->get\_socket() you load IO::Handle, which probably brings a lot of
-code with it, thereby obliterating a lot of the use for libghttp. So
+`$r-`get\_socket()> you load [IO::Handle](https://metacpan.org/pod/IO::Handle), which probably brings a lot of
+code with it, thereby obliterating a lot of the use for [libghttp](http://ftp.gnome.org/pub/gnome/sources/libghttp). So
 use at your own risk :-)
 
 # AUTHOR
--- lib/HTTP/GHTTP.pm
+++ lib/HTTP/GHTTP.pm
@@ -3,7 +3,7 @@ package HTTP::GHTTP;
 use strict;
 use warnings;
 
-our $VERSION = '1.080';
+our $VERSION = '1.080_001';
 $VERSION = eval $VERSION;
 
 require DynaLoader;
@@ -88,10 +88,12 @@ __END__
 
 =head1 NAME
 
-HTTP::GHTTP - Perl interface to the gnome ghttp library
+HTTP::GHTTP - (DEPRECATED) Perl interface to the gnome ghttp library
 
 =head1 SYNOPSIS
 
+  use strict;
+  use warnings;
   use HTTP::GHTTP;
 
   my $r = HTTP::GHTTP->new();
@@ -101,157 +103,250 @@ HTTP::GHTTP - Perl interface to the gnome ghttp library
 
 =head1 DESCRIPTION
 
-This is a fairly low level interface to the Gnome project's libghttp,
+The GNOME L<libghttp|http://ftp.gnome.org/pub/gnome/sources/libghttp> project
+has been dead since 2009.  This, this library is deprecated.
+
+B<Do not use this module!>
+
+This is a fairly low level interface to L<libghttp|http://ftp.gnome.org/pub/gnome/sources/libghttp>,
 which allows you to process HTTP requests to HTTP servers. There also
 exists a slightly higher level interface - a simple get() function
 which takes a URI as a parameter. This is not exported by default, you
 have to ask for it explicitly.
 
-=head1 API
+=head1 FUNCTIONS
 
-=head2 HTTP::GHTTP->new([$uri, [%headers]])
+The L<HTTP::GHTTP> module makes the following standalone functions available.
+
+=head2 get
+
+  get($uri, [%headers])
+
+This does everything automatically for you, retrieving the body at
+the remote URI. Optionally pass in headers.
+
+=head1 METHODS
+
+The L<HTTP::GHTTP> module makes the following methods available.
+
+=head2 new
+
+  my $r = HTTP::GHTTP->new("http://axkit.com/", Connection => "close");
 
 Constructor function - creates a new GHTTP object. If supplied a URI
 it will automatically call set_uri for you. If you also supply a list
 of key/value pairs it will set those as headers:
 
-    my $r = HTTP::GHTTP->new(
-        "http://axkit.com/",
-        Connection => "close");
+=head2 current_status
 
-=head2 $r->set_uri($uri)
-
-This sets the URI for the request
-
-=head2 $r->set_header($header, $value)
-
-This sets an outgoing HTTP request header
-
-=head2 $r->set_type($type)
-
-This sets the request type. The request types themselves are constants
-that are not exported by default. To export them, specify the :methods
-option in the import list:
-
-    use HTTP::GHTTP qw/:methods/;
-    my $r = HTTP::GHTTP->new();
-    $r->set_uri('http://axkit.com/');
-    $r->set_type(METHOD_HEAD);
-    ...
-
-The available methods are:
-
-    METHOD_GET
-    METHOD_POST
-    METHOD_OPTIONS
-    METHOD_HEAD
-    METHOD_PUT
-    METHOD_DELETE
-    METHOD_TRACE
-    METHOD_CONNECT
-    METHOD_PROPFIND
-    METHOD_PROPPATCH
-    METHOD_MKCOL
-    METHOD_COPY
-    METHOD_MOVE
-    METHOD_LOCK
-    METHOD_UNLOCK
-
-Some of these are for DAV.
-
-=head2 $r->set_body($body)
-
-This sets the body of a request, useful in POST and some of the DAV
-request types.
-
-=head2 $r->process_request()
-
-This sends the actual request to the server
-
-=head2 $r->get_status()
-
-This returns 2 values, a status code (numeric) and a status reason
-phrase. A simple example of the return values would be (200, "OK").
-
-=head2 $r->get_header($header)
-
-This gets the value of an incoming HTTP response header
-
-=head2 $r->get_headers()
-
-Returns a list of all the response header names in the order they
-came back. This method is only available in libghttp 1.08 and later -
-perl Makefile.PL should have reported whether it found it or not.
-
-  my @headers = $r->get_headers;
-  print join("\n",
-        map { "$_: " . $r->get_header($_) } @headers), "\n\n";
-
-=head2 $r->get_body()
-
-This gets the body of the response
-
-=head2 $r->get_error()
-
-If the response failed for some reason, this returns a textual error
-
-=head2 $r->set_authinfo($user, $password)
-
-This sets an outgoing username and password for simple HTTP
-authentication
-
-=head2 $r->set_proxy($proxy)
-
-This sets your proxy server, use the form "http://proxy:port"
-
-=head2 $r->set_proxy_authinfo($user, $password)
-
-If you have set a proxy and your proxy requires a username and password
-you can set it with this.
-
-=head2 $r->prepare()
-
-This is a low level interface useful only when doing async downloads.
-See L<ASYNC OPERATION>.
-
-=head2 $r->process()
-
-This is a low level interface useful only when doing async downloads.
-See L<ASYNC OPERATION>.
-
-process returns undef for error, 1 for "in progress", and zero for
-"complete".
-
-=head2 $r->get_socket()
-
-Returns an IO::Handle object that is the currently in progress socket.
-Useful only when doing async downloads. There appears to be some corruption
-when using the socket to retrieve file contents on more recent libghttp's.
-
-=head2 $r->current_status()
+  $r->current_status()
 
 This is only useful in async mode. It returns 3 values: The current
 processing stage (0 = none, 1 = request, 2 = response headers,
 3 = response), the number of bytes read, and the number of bytes total.
 
-=head2 $r->set_async()
+=head2 get_body
+
+  my $rresponse = $r->get_body();
+
+This gets the body of the response
+
+=head2 get_error
+
+  my $error = $r->get_error();
+
+If the response failed for some reason, this returns a textual error.
+
+=head2 get_header
+
+  my $value = $r->get_header($header);
+
+This gets the value of an incoming HTTP response header.
+
+=head2 get_headers
+
+  my @headers = $r->get_headers;
+  print join("\n",
+    map { "$_: " . $r->get_header($_) } @headers), "\n\n";
+
+Returns a list of all the response header names in the order they
+came back. This method is only available in L<libghttp|http://ftp.gnome.org/pub/gnome/sources/libghttp> 1.08 and later -
+C<perl Makefile.PL> should have reported whether it found it or not.
+
+=head2 get_socket
+
+  my $sock_h = $r->get_socket();
+
+Returns an L<IO::Handle> object that is the currently in progress socket.
+Useful only when doing async downloads. There appears to be some corruption
+when using the socket to retrieve file contents on more recent L<libghttp|http://ftp.gnome.org/pub/gnome/sources/libghttp>.
+
+=head2 get_status
+
+  $r->get_status()
+
+This returns 2 values, a status code (numeric) and a status reason
+phrase. A simple example of the return values would be (200, "OK").
+
+=head2 prepare
+
+  $r->prepare()
+
+This is a low level interface useful only when doing async downloads.
+See L<ASYNC OPERATION>.
+
+=head2 process
+
+  $r->process()
+
+This is a low level interface useful only when doing async downloads.
+See L<ASYNC OPERATION>.
+
+process returns C<undef> for error, 1 for "in progress", and zero for
+"complete".
+
+=head2 process_request
+
+  $r->process_request()
+
+This sends the actual request to the server
+
+=head2 set_async
+
+  $r->set_async()
 
 This turns async mode on. There is no corresponding unset function.
 
-=head2 $r->set_chunksize($bytes)
+=head2 set_authinfo
+
+  $r->set_authinfo($user, $password)
+
+This sets an outgoing username and password for simple HTTP
+authentication.
+
+=head2 set_body
+
+  $r->set_body($body);
+
+This sets the body of a request, useful in C<POST> and some of the DAV
+request types.
+
+=head2 set_chunksize
+
+  $r->set_chunksize($bytes)
 
 Sets the download (and upload) chunk size in bytes for use in async
 mode. This may be a useful value to set for slow modems, or perhaps
 for a download progress bar, or just to allow periodic writes.
 
-=head2 get($uri, [%headers])
+=head2 set_header
 
-This does everything automatically for you, retrieving the body at
-the remote URI. Optionally pass in headers.
+  $r->set_header($header_name, $header_value);
+  $r->set_header('Connection', 'Close');
+
+This sets an outgoing HTTP request header.
+
+=head2 set_proxy
+
+  $r->set_proxy("http://proxy:port")
+
+This sets your proxy server.
+
+=head2 set_proxy_authinfo
+
+  $r->set_proxy_authinfo($user, $password)
+
+If you have set a proxy and your proxy requires a username and password
+you can set it with this.
+
+=head2 set_type
+
+  use HTTP::GHTTP qw/:methods/;
+  my $r = HTTP::GHTTP->new();
+  $r->set_uri('http://axkit.com/');
+  $r->set_type(METHOD_HEAD);
+
+This sets the request type. The request types themselves are constants
+that are not exported by default. To export them, specify the C<:methods>
+option in the import list:
+
+
+The available methods are:
+
+=over
+
+=item *
+
+METHOD_GET
+
+=item *
+
+METHOD_POST
+
+=item *
+
+METHOD_OPTIONS
+
+=item *
+
+METHOD_HEAD
+
+=item *
+
+METHOD_PUT
+
+=item *
+
+METHOD_DELETE
+
+=item *
+
+METHOD_TRACE
+
+=item *
+
+METHOD_CONNECT
+
+=item *
+
+METHOD_PROPFIND
+
+=item *
+
+METHOD_PROPPATCH
+
+=item *
+
+METHOD_MKCOL
+
+=item *
+
+METHOD_COPY
+
+=item *
+
+METHOD_MOVE
+
+=item *
+
+METHOD_LOCK
+
+=item *
+
+METHOD_UNLOCK
+
+=back
+
+=head2 set_uri
+
+  my $boolean = $r->set_uri($some_URI_string);
+
+This sets the URI for the request.  It returns true if the URI was
+properly set and undef otherwise.
 
 =head1 ASYNC OPERATION
 
-Its possible to use an asynchronous mode of operation with ghttp. Here's
+It's possible to use an asynchronous mode of operation with L<HTTP::GHTTP>. Here's
 a brief example of how:
 
     my $r = HTTP::GHTTP->new("http://axkit.org/");
@@ -273,10 +368,10 @@ a brief example of how:
 Doing timeouts is an exercise for the reader (hint: lookup select() in
 perlfunc).
 
-Note also that $sock above is an IO::Handle, not an IO::Socket, although
+Note also that $sock above is an L<IO::Handle>, not an L<IO::Socket>, although
 you can probably get away with re-blessing it. Also note that by calling
-$r->get_socket() you load IO::Handle, which probably brings a lot of
-code with it, thereby obliterating a lot of the use for libghttp. So
+C<$r->get_socket()> you load L<IO::Handle>, which probably brings a lot of
+code with it, thereby obliterating a lot of the use for L<libghttp|http://ftp.gnome.org/pub/gnome/sources/libghttp>. So
 use at your own risk :-)
 
 =head1 AUTHOR
